8c25b8982a8263a2c500ff1f14ab0c563051e8ad
Anton Kuznetsov <kuznetsov74@gmail.com>
2017-06-07 22:37:35 -0400

    References to projects

---
+++ LearningManagment.Core/Constants/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LearningManagment.Core.Constants
+{
+    public class Constants
+    {
+        public static string HOME_PAGE = "Home Page!";
+    }
+}
--- LearningManagment.Web/Controllers/HomeController.cs
+++ LearningManagment.Web/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using LearningManagment.Core.Constants;
 
 namespace LearningManagment.Web.Controllers
 {
@@ -10,6 +7,7 @@ namespace LearningManagment.Web.Controllers
     {
         public IActionResult Index()
         {
+            ViewBag.Title = Constants.HOME_PAGE;
             return View();
         }
 
